14baa7f585ab7ddaae4165f3e57be981c49758c7
shriya96 <sharmashriya3095@gmail.com>
2023-06-13 21:58:04 +0530

    [Draft] Assignment 7.4

---
+++ Week-7/Assignment7.4/Assignment7_4.js
@@ -0,0 +1,44 @@
+prices=[7,1,1,1,1]
+function WhenToBuyStock(arr){
+	min=minVal(prices)[0]
+  MinIndex=minVal(prices)[1]
+
+  newArray=prices.slice(MinIndex,prices.length)
+
+  max=maxVal(newArray)[0]
+
+  if (max<=min){
+    profit = 0
+  }else{
+    profit = max-min
+  }
+	return profit
+}
+
+function maxVal(arr){
+	max=0
+  index=0
+  for (i=0;i<arr.length-1;i++){
+    if (arr[i]>max){
+			max=arr[i]
+      index=i
+    }
+  }
+  if (max==arr[0]){
+  	return [0,0]
+  }
+return [max,index]  
+}
+
+
+function minVal(arr){
+	min=arr[0]
+  index=0
+  for (i=0;i<arr.length-1;i++){
+    if (arr[i]<min){
+			min=arr[i]
+      index=i
+    }
+  }
+return [min,index]  
+}
